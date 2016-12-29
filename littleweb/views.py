5866b0b9063c7a9aae910186a0e3f81a303a756a
qinjing <qinjing@bilibili.com>
2016-12-29 19:25:28 +0800

    明白了调用格式

--- littleweb/views.py
+++ littleweb/views.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-from django.shortcuts import render
+from django.shortcuts import render_to_response,render
 from django.http import HttpResponse
 
 # Create your views here.
@@ -12,3 +12,4 @@ def add(request,a,b):
     b = request.GET.get("b",0)
     c = int(a)+int(b)
     return HttpResponse(str(c))
+
